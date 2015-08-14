3763c19ad7e834ab9655c1524127b3f6dbd3405a
Heath Barnett <heath.barnett@gmail.com>
2015-08-14 15:44:26 -0500

    reorder works
    TODO

--- inventory.py
+++ inventory.py
@@ -16,20 +16,34 @@ result = pd.merge(master, inv, how='left',on='Name',suffixes=(' 1', ' 2'))
 df=result.groupby(['Lab','Experiment'])
 
 
+def labmat(df):
+    for (section, exp), group in df:
+        print('Chemistry', section, '- Laboratory', exp)
+        print(group[['Name', 'Required', 'Unit']].to_string(index=False))
+        print('\n')
+
+
+print(labmat(df))
 def status(dX):
     if (dX['Stock'] - dX['Required']) / dX['Required'] > 1.2:
         return 'No'
     else:
         return 'Re-Order'
-invCompare = result[['Name','Lab','Unit','Required','Stock']]
+
+
+invCompare = result[['Name', 'Lab', 'Unit', 'Required', 'Stock', 'Type']]
+
 compare = pd.DataFrame({'Name': invCompare['Name'], 'Status': invCompare['Stock'] >= invCompare['Required'],'Projection': invCompare['Stock']-invCompare['Required']*4})
 
 invCompare = pd.merge(invCompare,compare,how='left')
 dd = invCompare
-gnoes = pd.DataFrame(
-    {'Name': invCompare['Name'], 'Stock': dd['Stock'], 'Required': dd['Required'], 'Pojection': dd['Projection'] <= 0,
-     'Remaining': dd['Stock'] - dd['Required'], 'Status': dd.apply(status, axis=1)})
-print(gnoes.to_string())
+
+gnoes = pd.DataFrame({'Name': invCompare['Name'], 'Type': dd['Type'], 'Stock': dd['Stock'], 'Required': dd['Required'],
+                      'Status': dd.apply(status, axis=1)})
+
+print(gnoes[(gnoes.Status == 'Re-Order') & (gnoes.Type == 'Reagent')].to_string())
+
+
 #print(invCompare[['Name','Lab','Unit','Required','Stock','Status','Projection']].to_string())
 
 
